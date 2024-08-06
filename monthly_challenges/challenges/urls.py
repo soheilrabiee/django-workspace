d985d2231745ddaf444b1982dfa8bf8edc00d927
Soheil <iamsoheil.r@gmail.com>
2024-08-06 19:18:46 +0330

    Add view and url paths for challenges

---
+++ monthly_challenges/challenges/urls.py
@@ -0,0 +1,4 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [path("<month>", views.monthly_challenge)]
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponseNotFound, HttpResponseRedirect, HttpResponse
+from django.urls import reverse
 
+monthly_challenges_text = {
+    "january": "Here is the challenge for january!",
+    "february": "Here is the challenge for february!",
+    "march": "Here is the challenge for march!",
+    "april": "Here is the challenge for april!",
+    "may": "Here is the challenge for may!",
+    "june": "Here is the challenge for june!",
+    "july": "Here is the challenge for july!",
+    "august": "Here is the challenge for august!",
+    "september": "Here is the challenge for september!",
+    "october": "Here is the challenge for october!",
+    "november": "Here is the challenge for november!",
+    "december": "Here is the challenge for december!",
+}
 # Create your views here.
+
+
+def monthly_challenge(request, month):
+    if month in monthly_challenges_text:
+        return HttpResponse(f"{monthly_challenges_text[month]}")
+    return HttpResponseNotFound("Incorrect month!")
