a98a74d1348700d8a5ceded4321dfff843987012
Soheil <iamsoheil.r@gmail.com>
2024-08-08 17:52:14 +0330

    Add render shortcut

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseNotFound, HttpResponseRedirect, HttpResponse
 from django.urls import reverse
-from django.template.loader import render_to_string
 
 monthly_challenges_text = {
     "january": "Here is the challenge for january!",
@@ -37,7 +36,7 @@ def monthly_challenge(request, month):
     """Checks the dict and returns the value based of month"""
     try:
         output = monthly_challenges_text[month]
-        return HttpResponse(render_to_string("challenges/challenge.html"))
+        return render(request, "challenges/challenge.html")
     except:
         return HttpResponseNotFound("<h1>Invalid month!</h1>")
 
