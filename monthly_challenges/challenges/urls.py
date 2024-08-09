0e51cd5f3c7266d05da3c7abc6945cd3316e84ef
Soheil <iamsoheil.r@gmail.com>
2024-08-09 18:27:24 +0330

    Add navbar include snippet

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 # It's important to add int:month path first
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name="index"),
     path("<int:month>", views.monthly_challenge_by_number),
     path("<str:month>", views.monthly_challenge, name="month-challenge"),
 ]
